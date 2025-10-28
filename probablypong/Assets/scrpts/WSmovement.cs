8b6e948925f4ba4eb7e22a290102b77b43438739
Kieran <kieranm0104@gmail.com>
2025-10-28 14:42:55 -0400

    madethe platfor move
    made it move man

---
+++ probablypong/Assets/scrpts/WSmovement.cs
@@ -0,0 +1,32 @@
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WSmovement : MonoBehaviour
+{
+    //variables
+
+    public Rigidbody2D rb;
+
+    public float movespeed;
+
+    private Vector2 direction;
+
+    public InputActionReference move;
+
+
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(y: direction.y * movespeed, x: direction.x = 0);
+    }
+
+}
